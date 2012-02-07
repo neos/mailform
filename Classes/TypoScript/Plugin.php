adecb7451d8590098b63306a699ae1d269bc6f75
Karsten Dambekalns <karsten@typo3.org>
2012-02-07 15:45:36 +0100

    [TASK] Clean up docblocks a little
    Change-Id: I9920dee7a0186a8c2a74d8a7c6a9859bb5a945c9

--- Classes/TypoScript/Plugin.php
+++ Classes/TypoScript/Plugin.php
@@ -5,18 +5,8 @@ namespace TYPO3\MailForm\TypoScript;
  * This script belongs to the FLOW3 package "MailForm".                   *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License as published by the Free   *
- * Software Foundation, either version 3 of the License, or (at your      *
- * option) any later version.                                             *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
- * Public License for more details.                                       *
- *                                                                        *
- * You should have received a copy of the GNU General Public License      *
- * along with the script.                                                 *
- * If not, see http://www.gnu.org/licenses/gpl.html                       *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
@@ -24,8 +14,6 @@ namespace TYPO3\MailForm\TypoScript;
 use TYPO3\FLOW3\Annotations as FLOW3;
 
 /**
- *
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  * @FLOW3\Scope("prototype")
  */
 class Plugin extends \TYPO3\TYPO3\TypoScript\Plugin {
